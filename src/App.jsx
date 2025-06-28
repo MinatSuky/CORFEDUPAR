9251738f4cf70e64627ffe00f66d0bea6f86a8f9
Sebastian Gomez <sebastianelproblematico@gmail.com>
2025-06-28 11:56:05 -0500

    commet #3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import feria3 from "../src/IMGS/feria3.JPG"
 import feria4 from "../src/IMGS/feria4.JPG"
 import feria5 from "../src/IMGS/feria5.JPG"
 import plaza from "../src/IMGS/sesion1.2.png"
-import MobileSesion1 from "../src/IMGS/sesion1 Mobile.png"
+import MobileSesion1 from "../src/IMGS/sesion1 Mobile-2.png"
 import sesion3 from "../src/IMGS/sesion3-1.png"
 import ser1 from "../src/IMGS/servicio 1.jpg"
 import ser2 from "../src/IMGS/servicio 2.jpeg"
@@ -23,23 +23,23 @@ function App() {
       {/* --------- sesion #1 ---------- */}
       <div className='w-[100%] flex flex-col items-center justify-center relative'>
         <img className='w-full h-full object-cover shadow-2xl hidden md:flex' src={plaza} alt="" />
-        <img className='w-full h-full object-cover shadow-2xl flex md:hiden' src={MobileSesion1} alt="" />
+        <img className='w-full h-full object-cover shadow-2xl flex md:hidden' src={MobileSesion1} alt="" />
       </div>
       {/* --------- sesion #2 ---------- */}
       <p className='text-base-verde text-4xl font-bold text-center mt-16'>¿Quienes Somos?</p>
-      <div className='sm:flex sm:flex-col xl:flex-row mt-10 gap-16 mx-10'>
+      <div className='sm:flex sm:flex-col xl:flex-row mt-10 gap-16'>
         {/* Columna izquierda */}
-        <div className='w-[100%] sm:w-[100%] xl:w-[50%] flex flex-col justify-center items-center gap-4'>
+        <div className='w-[100%] sm:w-[100%] xl:w-[50%] flex flex-col justify-center items-center gap-2 lg:gap-4'>
 
           {/* Imagen grande */}
-          <div className='flex gap-4 px-4'>
-            <div className="w-full overflow-hidden shadow-md shadow-gray-400 rounded-3xl border border-gray-300">
+          <div className='flex gap-2 lg:gap-4 px-4'>
+            <div className="w-full overflow-hidden shadow-md shadow-gray-400 rounded-md md:rounded-3xl border border-gray-300">
               <img className=' w-full h-full object-cover '
                 src={feria5}
                 alt=""
               />
             </div>
-            <div className="w-[50%] sm:w-[50%] xl:w-auto overflow-hidden shadow-md shadow-gray-400 rounded-3xl border border-gray-300">
+            <div className="w-[50%] sm:w-[50%] xl:w-auto overflow-hidden shadow-md shadow-gray-400 rounded-md md:rounded-3xl border border-gray-300">
               <img className=' w-full h-full object-cover'
                 src={feria1}
                 alt=""
@@ -48,14 +48,14 @@ function App() {
           </div>
 
           {/* Imágenes pequeñas en fila */}
-          <div className='flex gap-4 px-4'>
-            <div className="w-[50%] sm:w-[50%] xl:w-[90%] overflow-hidden shadow-md shadow-gray-400 rounded-3xl border border-gray-300">
+          <div className='flex gap-2 lg:gap-4 px-4'>
+            <div className="w-[50%] sm:w-[50%] xl:w-[90%] overflow-hidden shadow-md shadow-gray-400 rounded-md md:rounded-3xl border border-gray-300">
               <img className='w-full h-full object-cover'
                 src={feria3}
                 alt=""
               />
             </div>
-            <div className="w-full  overflow-hidden shadow-md shadow-gray-400 rounded-3xl border border-gray-300">
+            <div className="w-full overflow-hidden shadow-md shadow-gray-400 rounded-md md:rounded-3xl border border-gray-300">
               <img className=' w-full h-full object-cover'
                 src={feria4}
                 alt=""
@@ -65,27 +65,29 @@ function App() {
         </div>
 
         {/* Columna derecha */}
-        <div className='w-[100%] mt-10 xl:mt-0 items-center sm:w-[100%] sm:items-center xl:w-[50%] flex justify-around flex-col px-10'>
+        <div className='w-[100%] mt-10 xl:mt-0 items-center sm:w-[100%] sm:items-center xl:w-[50%] flex justify-around flex-col px-3 md:px-10'>
 
-          <p className='mt-4'> <span className='font-bold'>LA CORPORACION AGROPECUARIA DE FERIAS Y EXPOSICIONES DE VALLEDUPAR – CORFEDUPAR,</span> es una entidad privada sin ánimo de lucro, persona jurídica  con numero de matricula 0500451 inscrita en la cámara de comercio de Valledupar desde el 04 de febrero de 1998, pero quien llevo a cabo su primer evento ferial en abril del año 1975 alterno con el Festival vallenato,  marcando  el inicio de lo que ha sido este evento, quien desde entonces ha venido demostrando que es la vitrina vendedora de la costa, donde año tras año vincula a los ganaderos del cesar y del resto del país para la muestra de sus hatos ganaderos y avances tecnológicos en sus predios, como también le permite a las empresas locales y nacionales  mostrar sus productos, servicios, avances e innovaciones.</p>
+          <p className='mt-4 text-[14px] md:text-lg'> <span className='font-bold'>LA CORPORACION AGROPECUARIA DE FERIAS Y EXPOSICIONES DE VALLEDUPAR – CORFEDUPAR,</span> es una entidad privada sin ánimo de lucro, persona jurídica  con numero de matricula 0500451 inscrita en la cámara de comercio de Valledupar desde el 04 de febrero de 1998, pero quien llevo a cabo su primer evento ferial en abril del año 1975 alterno con el Festival vallenato,  marcando  el inicio de lo que ha sido este evento, quien desde entonces ha venido demostrando que es la vitrina vendedora de la costa, donde año tras año vincula a los ganaderos del cesar y del resto del país para la muestra de sus hatos ganaderos y avances tecnológicos en sus predios, como también le permite a las empresas locales y nacionales  mostrar sus productos, servicios, avances e innovaciones.</p>
 
-          <div className='flex gap-3 items-start  mt-8'>
+          <div className='flex flex-col md:flex-row gap-3 items-start mt-8'>
             <div className='ml-3'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 16 16"><path fill="#5e7b27" d="M16 2h-2V0h-2v2h-2v2h2v2h2V4h2z" /><path fill="#5e7b27" d="M13.498 6.969q.433.481.782 1.031a7.6 7.6 0 0 1-2.335 2.348a7.33 7.33 0 0 1-7.889 0A7.6 7.6 0 0 1 1.721 8a7.6 7.6 0 0 1 2.52-2.462A4 4 0 1 0 12 6.907v-.032a4 4 0 0 1-2.999-3.817A9 9 0 0 0 8 3.001c-3.489 0-6.514 2.032-8 5c1.486 2.968 4.511 5 8 5s6.514-2.032 8-5a9.2 9.2 0 0 0-.979-1.548a4 4 0 0 1-1.523.517zM6.5 5a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 6.5 5" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 16 16"><path fill="#439539" d="M16 2h-2V0h-2v2h-2v2h2v2h2V4h2z" /><path fill="#439539" d="M13.498 6.969q.433.481.782 1.031a7.6 7.6 0 0 1-2.335 2.348a7.33 7.33 0 0 1-7.889 0A7.6 7.6 0 0 1 1.721 8a7.6 7.6 0 0 1 2.52-2.462A4 4 0 1 0 12 6.907v-.032a4 4 0 0 1-2.999-3.817A9 9 0 0 0 8 3.001c-3.489 0-6.514 2.032-8 5c1.486 2.968 4.511 5 8 5s6.514-2.032 8-5a9.2 9.2 0 0 0-.979-1.548a4 4 0 0 1-1.523.517zM6.5 5a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 6.5 5" /></svg>
+              <p className='text-base-verde text-xl font-bold md:hidden flex'>VISIÓN</p>
             </div>
             <div >
-              <p className='text-base-verde text-xl font-bold'>VISIÓN</p>
-              <p className='mt-4'>Celebrar periódicamente eventos feriales bovinos, equinos, y ovino- caprino , Promover y difundir información  útil sobre  el desarrollo de la ganadería y sus cruces en Colombia, fomentar y colaborar con asesorías ganaderas y de organización  a municipios, veredas en el departamento del cesar que realicen eventos feriales, además de la promoción del  desarrollo empresarial y el emprendimiento en actividades sociales, folclóricas y culturales</p>
+              <p className='text-base-verde text-xl font-bold hidden md:flex'>VISIÓN</p>
+              <p className='mt-4 text-[14px] md:text-lg'>Celebrar periódicamente eventos feriales bovinos, equinos, y ovino- caprino , Promover y difundir información  útil sobre  el desarrollo de la ganadería y sus cruces en Colombia, fomentar y colaborar con asesorías ganaderas y de organización  a municipios, veredas en el departamento del cesar que realicen eventos feriales, además de la promoción del  desarrollo empresarial y el emprendimiento en actividades sociales, folclóricas y culturales</p>
             </div>
 
           </div>
-          <div className='flex gap-3 items-start  mt-8'>
+          <div className='flex flex-col md:flex-row gap-3 items-start mt-8'>
             <div>
-              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024"><path fill="#5e7b27" d="m772.13 452.84l53.86-351.81c1.32-10.01-1.17-18.68-7.49-26.02S804.35 64 795.01 64H228.99v-.01h-.06c-9.33 0-17.15 3.67-23.49 11.01s-8.83 16.01-7.49 26.02l53.87 351.89C213.54 505.73 193.59 568.09 192 640c2 90.67 33.17 166.17 93.5 226.5S421.33 957.99 512 960c90.67-2 166.17-33.17 226.5-93.5c60.33-60.34 91.49-135.83 93.5-226.5c-1.59-71.94-21.56-134.32-59.87-187.16M640.01 128h117.02l-39.01 254.02c-20.75-10.64-40.74-19.73-59.94-27.28c-5.92-3-11.95-5.8-18.08-8.41V128zM576 128v198.76c-13.18-2.58-26.74-4.43-40.67-5.55c-8.07-.8-15.85-1.2-23.33-1.2c-10.54 0-21.09.66-31.64 1.96a360 360 0 0 0-32.36 4.79V128zm-192 0h.04v218.3c-6.22 2.66-12.34 5.5-18.36 8.56c-19.13 7.54-39.02 16.6-59.66 27.16L267.01 128zm308.99 692.99c-48 48-108.33 73-180.99 75.01c-72.66-2.01-132.99-27.01-180.99-75.01S258.01 712.66 256 640c2.01-72.66 27.01-132.99 75.01-180.99c19.67-19.67 41.41-35.47 65.22-47.41c38.33-15.04 71.15-23.92 98.44-26.65c5.07-.41 10.2-.7 15.39-.88c.63-.01 1.28-.03 1.91-.03c.66 0 1.35.03 2.02.04c5.11.17 10.15.46 15.13.86c27.4 2.71 60.37 11.65 98.91 26.79c23.71 11.93 45.36 27.69 64.96 47.29c48 48 73 108.33 75.01 180.99c-2.01 72.65-27.01 132.98-75.01 180.98" /><path fill="#5e7b27" d="M544 480H416v64h64v192h-64v64h192v-64h-64z" /></svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 1024 1024"><path fill="#439539" d="m772.13 452.84l53.86-351.81c1.32-10.01-1.17-18.68-7.49-26.02S804.35 64 795.01 64H228.99v-.01h-.06c-9.33 0-17.15 3.67-23.49 11.01s-8.83 16.01-7.49 26.02l53.87 351.89C213.54 505.73 193.59 568.09 192 640c2 90.67 33.17 166.17 93.5 226.5S421.33 957.99 512 960c90.67-2 166.17-33.17 226.5-93.5c60.33-60.34 91.49-135.83 93.5-226.5c-1.59-71.94-21.56-134.32-59.87-187.16M640.01 128h117.02l-39.01 254.02c-20.75-10.64-40.74-19.73-59.94-27.28c-5.92-3-11.95-5.8-18.08-8.41V128zM576 128v198.76c-13.18-2.58-26.74-4.43-40.67-5.55c-8.07-.8-15.85-1.2-23.33-1.2c-10.54 0-21.09.66-31.64 1.96a360 360 0 0 0-32.36 4.79V128zm-192 0h.04v218.3c-6.22 2.66-12.34 5.5-18.36 8.56c-19.13 7.54-39.02 16.6-59.66 27.16L267.01 128zm308.99 692.99c-48 48-108.33 73-180.99 75.01c-72.66-2.01-132.99-27.01-180.99-75.01S258.01 712.66 256 640c2.01-72.66 27.01-132.99 75.01-180.99c19.67-19.67 41.41-35.47 65.22-47.41c38.33-15.04 71.15-23.92 98.44-26.65c5.07-.41 10.2-.7 15.39-.88c.63-.01 1.28-.03 1.91-.03c.66 0 1.35.03 2.02.04c5.11.17 10.15.46 15.13.86c27.4 2.71 60.37 11.65 98.91 26.79c23.71 11.93 45.36 27.69 64.96 47.29c48 48 73 108.33 75.01 180.99c-2.01 72.65-27.01 132.98-75.01 180.98" /><path fill="#5e7b27" d="M544 480H416v64h64v192h-64v64h192v-64h-64z" /></svg>
+              <p className='text-base-verde text-xl font-bold md:hidden flex'>MISIÓN</p>
             </div>
             <div>
-              <p className='text-base-verde text-xl font-bold'>MISIÓN</p>
-              <p className='mt-4'>Ser líder en el departamento del Cesar, en la prestación de servicios de subastas ganaderas, garantizando al ganadero  local y nacional la compra de ejemplares de calidad, con responsabilidad y garantías, además lograr posesionar a sus eventos feriales marcas nacionales que quieran darse a conocer, lo cual nos permitiría seguir aportando a nuestro departamento ingresos económicos en hotelería, transporte, restaurantes, almacenes veterinarios entre otros, con la llegada de foráneos.</p>
+              <p className='text-base-verde text-xl font-bold hidden md:flex'>MISIÓN</p>
+              <p className='mt-4 text-[14px] md:text-lg'>Ser líder en el departamento del Cesar, en la prestación de servicios de subastas ganaderas, garantizando al ganadero  local y nacional la compra de ejemplares de calidad, con responsabilidad y garantías, además lograr posesionar a sus eventos feriales marcas nacionales que quieran darse a conocer, lo cual nos permitiría seguir aportando a nuestro departamento ingresos económicos en hotelería, transporte, restaurantes, almacenes veterinarios entre otros, con la llegada de foráneos.</p>
             </div>
 
           </div>
--- src/componentes/BarraPrincipal.jsx
+++ src/componentes/BarraPrincipal.jsx
@@ -29,20 +29,20 @@ export const BarraPrincipal = () => {
 
       </div>
       <div className='bg-base-verde-oscuro flex justify-around items-center'>
-        <div className='redes-sociales flex gap-2'>
-          <p className='border border-white rounded-full py-3 px-3 my-3 shadoow'>
+        <div className='redes-sociales flex items-center gap-2'>
+          <p className='border w-full border-white rounded-full py-1 px-1 md:py-3 md:px-3 my-3 shadoow'>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="M13.355 22v-9.123h3.062l.459-3.555h-3.52v-2.27c0-1.03.285-1.731 1.761-1.731L17 5.32V2.14A25 25 0 0 0 14.257 2c-2.715 0-4.573 1.657-4.573 4.7v2.622h-3.07v3.555h3.07V22z" /></svg>
           </p>
-          <p className='border border-white rounded-full py-3 px-3 my-3 shadoow'>
+          <p className='border border-white rounded-full py-1 px-1 md:py-3 md:px-3 my-3 shadoow'>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3" /></svg>
           </p>
-          <p className='border border-white rounded-full py-3 px-3 my-3 shadoow'>
+          <p className='border border-white rounded-full py-1 px-1 md:py-3 md:px-3 my-3 shadoow'>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 256 256"><path fill="#ffffff" d="M215 219.85a8 8 0 0 1-7 4.15h-48a8 8 0 0 1-6.75-3.71l-40.49-63.63l-58.84 64.72a8 8 0 0 1-11.84-10.76l61.77-68l-62.6-98.32A8 8 0 0 1 48 32h48a8 8 0 0 1 6.75 3.71l40.49 63.63l58.84-64.72a8 8 0 0 1 11.84 10.76l-61.77 67.95l62.6 98.38a8 8 0 0 1 .25 8.14" /></svg>
           </p>
-          <p className='border border-white rounded-full py-3 px-3 my-3 shadoow'>
+          <p className='border border-white rounded-full py-1 px-1 md:py-3 md:px-3 my-3 shadoow'>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="m10 15l5.19-3L10 9zm11.56-7.83c.13.47.22 1.1.28 1.9c.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83c-.25.9-.83 1.48-1.73 1.73c-.47.13-1.33.22-2.65.28c-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44c-.9-.25-1.48-.83-1.73-1.73c-.13-.47-.22-1.1-.28-1.9c-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83c.25-.9.83-1.48 1.73-1.73c.47-.13 1.33-.22 2.65-.28c1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44c.9.25 1.48.83 1.73 1.73" /></svg>
           </p>
-          <p className='border border-white rounded-full py-3 px-3 my-3 shadoow'>
+          <p className='border border-white rounded-full py-1 px-1 md:py-3 md:px-3 my-3 shadoow'>
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><mask id="IconifyId197b70931a2ad8fbe1"><g fill="none" stroke="#fff" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path fill="#fff" stroke="none" d="M16.6 5.82c-0.68 -0.78 -1.06 -1.78 -1.06 -2.82h-3.09v12.4c-0.02 0.67 -0.31 1.31 -0.79 1.77c-0.48 0.47 -1.13 0.73 -1.8 0.73c-1.42 0 -2.6 -1.16 -2.6 -2.6c0 -1.72 1.66 -3.01 3.37 -2.48v-3.16c-3.45 -0.46 -6.47 2.22 -6.47 5.64c0 3.33 2.76 5.7 5.69 5.7c3.14 0 5.69 -2.55 5.69 -5.7v-6.29c1.25 0.9 2.76 1.38 4.3 1.38v-3.09c0 0 -1.88 0.09 -3.24 -1.48Z" /><path stroke="#000" stroke-dasharray="36" stroke-dashoffset="72" stroke-width="4" d="M11 11h-1c-2.21 0 -4.5 1.79 -4.5 4c0 2.21 1.5 4.5 4.5 4.5c2.21 0 4 -2.29 4 -4.5v-12.5"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="72;36" /></path><path stroke="#000" stroke-dasharray="10" stroke-dashoffset="20" stroke-width="4" d="M18 2.5v8"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.5s" dur="0.1s" values="20;10" /></path></g></mask><rect width="24" height="24" fill="#ffffff" mask="url(#IconifyId197b70931a2ad8fbe1)" /></svg>
           </p>
         </div>
