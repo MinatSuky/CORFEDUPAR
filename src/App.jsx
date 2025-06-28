aa23192f47948224159d6e4c0dab15274a3c66bf
Sebastian Gomez <sebastianelproblematico@gmail.com>
2025-06-28 16:08:53 -0500

    commit #5

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,10 @@ import plaza from "../src/IMGS/sesion1.2.png"
 import MobileSesion1 from "../src/IMGS/sesion1 Mobile-2.png"
 import MobileSesion3 from "../src/IMGS/sesion3 mobile.png"
 import sesion3 from "../src/IMGS/sesion3-1.png"
+import logoBlanco from "../src/IMGS/Logo_png.png"
 // Servicios
 import ser1 from "../src/IMGS/servicio 1.jpg"
-import ser2 from "../src/IMGS/servicio 2.jpeg"
+import ser2 from "../src/IMGS/subasta.JPG"
 import ser3 from "../src/IMGS/servicio 3.jpg"
 import ser4 from "../src/IMGS/servicio 4.jpg"
 import ser5 from "../src/IMGS/servicio 5.jpg"
@@ -22,7 +23,16 @@ import fidegan from "../src/IMGS/fedegan.png"
 import subagan from "../src/IMGS/subagan.svg"
 import ovicer from "../src/IMGS/Ovicer.png"
 // Galeria de imagenes
-
+import Foto1 from "../src/IMGS/galeria1.JPG"
+import Foto2 from "../src/IMGS/galeria2.JPG"
+import Foto3 from "../src/IMGS/galeria3.JPG"
+import Foto4 from "../src/IMGS/galeria4.JPG"
+import Foto5 from "../src/IMGS/galeria5.JPG"
+import Foto6 from "../src/IMGS/galeria6.JPG"
+import Foto7 from "../src/IMGS/galeria7.JPG"
+import Foto8 from "../src/IMGS/galeria8.JPG"
+import Foto9 from "../src/IMGS/galeria9.JPG"
+import Foto10 from "../src/IMGS/galeria10.JPG"
 
 function App() {
 
@@ -112,7 +122,7 @@ function App() {
       {/* --------- sesion #4 ---------- */}
       <div className='flex flex-col items-center mt-20'>
         <p className='text-base-verde font-bold text-4xl mt-6'>Nuestros Servicios</p>
-        <div className="w-full flex justify-around overflow-x-auto">
+        <div className="w-full flex justify-around ocultar-scroll overflow-x-auto">
           <div className="flex w-max gap-5 mt-10 px-4 py-6 mb-10">
             <CardSesion3 IMG={ser4} text="•	CAPACITACÓN GENERAL" svg={<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 20 20"><path fill="#65a30d" d="M3.33 8L10 12l10-6l-10-6L0 6h10v2zM0 8v8l2-2.22V9.2zm10 12l-5-3l-2-1.2v-6l7 4.2l7-4.2v6z"/></svg>} />
             <CardSesion3 IMG={ser1} text="•	EVENTOS FERIALES ANUALES" svg={<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path fill="#65a30d" d="M5.24 14.42c-.04 1.76-.18 3.72-.58 5.58h2.67c.27-1.94.43-4.07.52-5.93c-.31-.17-.6-.37-.85-.63c-.47.49-1.08.83-1.76.98M11 11H8c0 .83.67 1.5 1.5 1.5S11 11.83 11 11m-5 0H3c0 .83.67 1.5 1.5 1.5S6 11.83 6 11m6-6.29C10.67 5.85 8.51 7.55 5.89 9h12.23C15.49 7.55 13.33 5.85 12 4.71m7.5 7.79c.83 0 1.5-.67 1.5-1.5h-3c0 .83.67 1.5 1.5 1.5m-5 0c.83 0 1.5-.67 1.5-1.5h-3c0 .83.67 1.5 1.5 1.5m2.5.94c-.25.25-.54.45-.85.62c.1 1.87.26 4 .52 5.93h2.67c-.39-1.86-.54-3.82-.57-5.58c-.69-.14-1.3-.48-1.77-.97" opacity=".3"/><path fill="#65a30d" d="M12 2S7 7 1 9v2c0 1.49.93 2.75 2.24 3.26C3.2 16.76 2.92 19.69 2 22h20c-.92-2.31-1.2-5.24-1.24-7.74A3.5 3.5 0 0 0 23 11V9c-6-2-11-7-11-7m0 2.71c1.33 1.14 3.49 2.84 6.11 4.29H5.89C8.51 7.55 10.67 5.85 12 4.71M3 11h3c0 .83-.67 1.5-1.5 1.5S3 11.83 3 11m4.33 9H4.66c.39-1.86.54-3.82.57-5.58c.68-.15 1.29-.49 1.76-.98c.25.25.54.45.85.62c-.08 1.87-.24 4-.51 5.94M8 11h3c0 .83-.67 1.5-1.5 1.5S8 11.83 8 11m1.35 9c.24-1.83.39-3.78.48-5.53c.84-.08 1.61-.45 2.17-1.02c.56.57 1.32.94 2.17 1.02c.1 1.75.24 3.7.48 5.53zm5.15-7.5c-.83 0-1.5-.67-1.5-1.5h3c0 .83-.67 1.5-1.5 1.5m2.17 7.5c-.27-1.94-.43-4.07-.52-5.93c.31-.17.61-.37.85-.62c.47.48 1.08.83 1.76.98c.03 1.76.18 3.72.57 5.58h-2.66zm2.83-7.5c-.83 0-1.5-.67-1.5-1.5h3c0 .83-.67 1.5-1.5 1.5"/></svg>} />
@@ -125,15 +135,59 @@ function App() {
        {/* --------- sesion #5 ---------- */}
         <div className='flex flex-col items-center mt-20'>
             <p className='text-base-verde font-bold text-4xl mt-6'>Nuestros Aliados</p>
-            <div className="w-full flex justify-around overflow-x-auto">
+            <div className="w-full flex justify-around ocultar-scroll overflow-x-auto">
                   <div className='flex w-max h-[200px] gap-10 mt-10 px-4 py-6 mb-10'>
-                    <img className='w-full' src={riendas} alt="" />
-                    <img className='w-fill' src={fidegan} alt="" />
-                    <img className='w-fill' src={subagan} alt="" />
-                    <img className='w-fill' src={ovicer} alt="" />
+                    <img className='w-full hover:scale-105 duration-200' src={riendas} alt="" />
+                    <img className='w-fill hover:scale-105 duration-200' src={fidegan} alt="" />
+                    <img className='w-fill hover:scale-105 duration-200' src={subagan} alt="" />
+                    <img className='w-fill hover:scale-105 duration-200' src={ovicer} alt="" />
                   </div>
             </div>
         </div>
+        {/* --------- sesion #6 ---------- */}
+       
+          <p className='text-base-verde font-bold text-4xl mt-20 text-center'>Galeria de Imagenes</p>
+        
+      <div className="galeria mx-0 md:mx-10 mt-20">
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto1} alt="Imagen 1"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto2} alt="Imagen 2"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto3} alt="Imagen 3"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto4} alt="Imagen 4"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto5} alt="Imagen 5"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto6} alt="Imagen 6"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto7} alt="Imagen 7"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto8} alt="Imagen 8"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto9} alt="Imagen 9"/>
+        <img className='hover:scale-105 duration-200 shadow-2xl' src={Foto10} alt="Imagen 10"/>
+      </div>
+      <footer className='bg-base-verde flex flex-col pt-20 mt-36'>
+          <div className='flex justify-around'>
+              <div className='flex flex-col items-center'>
+                    <img className='drop-shadow-xl w-[40px] md:w-[80px]' src={logoBlanco} alt="" />
+                    <p className='text-white  font-extrabold text-shado'>CORFEDUPAR</p>
+              </div>
+              <div>
+                    <div className='flex gap-3'>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="#ffffff" d="M19.527 4.799c1.212 2.608.937 5.678-.405 8.173c-1.101 2.047-2.744 3.74-4.098 5.614c-.619.858-1.244 1.75-1.669 2.727c-.141.325-.263.658-.383.992c-.121.333-.224.673-.34 1.008c-.109.314-.236.684-.627.687h-.007c-.466-.001-.579-.53-.695-.887c-.284-.874-.581-1.713-1.019-2.525c-.51-.944-1.145-1.817-1.79-2.671zM8.545 7.705l-3.959 4.707c.724 1.54 1.821 2.863 2.871 4.18q.371.465.737.936l4.984-5.925l-.029.01c-1.741.601-3.691-.291-4.392-1.987a3.4 3.4 0 0 1-.209-.716c-.063-.437-.077-.761-.004-1.198zM5.492 3.149l-.003.004c-1.947 2.466-2.281 5.88-1.117 8.77l4.785-5.689l-.058-.05zM14.661.436l-3.838 4.563l.027-.01c1.6-.551 3.403.15 4.22 1.626c.176.319.323.683.377 1.045c.068.446.085.773.012 1.22l-.003.016l3.836-4.561A8.38 8.38 0 0 0 14.67.439zM9.466 5.868L14.162.285l-.047-.012A8.3 8.3 0 0 0 11.986 0a8.44 8.44 0 0 0-6.169 2.766l-.016.018z"/></svg>
+                          <p className='text-[10px] md:text-[15px] text-white'>CRA 7ª KM 5 VIA LA PAZ 
+                            <br /> - COLISEO DE FERIAS
+                          <br /> PEDRO CASTRO MONSALVO.</p>
+                    </div>
+                    <div className='flex items-center gap-3 mt-2'>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 16 16"><path fill="#ffffff" d="M15 2H1c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h14c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1M4.94 9.94l-3 3c-.12.12-.28.18-.44.18s-.32-.06-.44-.18a.63.63 0 0 1 0-.88l3-3c.24-.24.64-.24.88 0c.25.24.25.64 0 .88m10 3c-.12.12-.28.18-.44.18s-.32-.06-.44-.18l-3-3a.63.63 0 0 1 0-.88c.24-.24.64-.24.88 0l3 3c.25.24.25.64 0 .88m0-9L8.8 10.09c-.22.22-.51.33-.8.33s-.58-.11-.8-.33L1.06 3.94c-.24-.24-.24-.64 0-.88s.64-.24.88 0L8 9.12l6.06-6.06c.24-.24.64-.24.88 0c.25.24.25.64 0 .88"/></svg>
+                          <p className='text-[10px] md:text-[15px] text-white'>corfedupar@yahoo.com</p>
+                    </div>
+              </div>
+          </div>
+          <p className='text-white px-10 text-center mt-5'>
+              Lugar del evento: Valledupar-Cesar, Colombia / Tipo del evento: Feria agropecuaria / Fecha del evento: Del 15 al 17 de Agosto de 2025 / Horario del evento para visitantes: 10:00 a.m. a 8:00 p.m. / CORFEDUPAR. Los organizadores de la feria no se hacen responsables de ninguna negociación.
+          </p>
+          <hr className='my-5 text-white font-bold'></hr>
+           <aside>
+            <p className='text-white text-center'>Copyright © {new Date().getFullYear()} CORFEDUPAR - All right reserved</p>
+          </aside>    
+      </footer>
+      
     </>
   )
 }
